579bcc850ce3ca993f9b372fb7b02da145b7209e
kerodim <turboboomer2000@gmail.com>
2020-03-13 10:38:38 +0300

    fix

--- src/Cli.php
+++ src/Cli.php
@@ -7,7 +7,7 @@ use function cli\prompt;
 
 function run()
 {
-    line('Welcometo the Brain Games!');
+    line('Welcometo the Brain Games!\nAnswer "yes" if the number is even, otherwise answer "no".');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
 }
