9395b6743776e907deaa7113b3fa762e9232934e
kerodim <turboboomer2000@gmail.com>
2020-03-29 17:38:35 +0300

    naming fix

--- src/Cli.php
+++ src/Cli.php
@@ -8,21 +8,21 @@ use function BrainGames\Logic\isEven;
 
 function run()
 {
-    line('Welcometo the Brain Games!');
+    line('Welcome to the Brain Games!');
     line('Answer "yes" if the number is even, otherwise answer "no".');
     line('');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
     line('');
-    $index = 3;
-    for ($index; $index >= 1;) {
+    $raunds = 3;
+    for ($raunds; $raunds >= 1;) {
         $number = rand(1, 100);
         line('Question: ' . $number);
         $userAnswer = prompt('Your answer');
         $correctAnswer = isEven($number);
         if ($userAnswer == $correctAnswer) {
             line('Correct!');
-            $index = $index - 1;
+            $raunds = $raunds - 1;
         } else {
             line("'" . $userAnswer . "' is wrong answer ;(. Correct answer was '" . $correctAnswer . "'.");
             line("Let's try again, " . $name . '!');
