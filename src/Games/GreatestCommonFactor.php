08e6a4636febd1c2fff964d4d7683f19ad2f2c13
kerodim <turboboomer2000@gmail.com>
2020-07-20 15:00:03 +0300

    change brain-gcd architecture

--- src/Games/GreatestCommonFactor.php
+++ src/Games/GreatestCommonFactor.php
@@ -2,6 +2,11 @@
 
 namespace Braingames\Games\GreatestCommonFactor;
 
+use function BrainGames\Cli\gameGreeting;
+use function BrainGames\Cli\isUserAnswerTrue;
+use function BrainGames\Cli\gameEnding;
+
+/*
 function showTaskToPlayerGCD()
 {
     return 'Find the greatest common divisor of given numbers.';
@@ -31,4 +36,36 @@ function calculationGCD($numbers)
         $remainder = $greaterNumber % $lowerNumber;
     }
     return (string) $lowerNumber;
+}
+*/
+
+function gcd()
+{
+    $gameDescription = 'Find the greatest common divisor of given numbers.';
+    $playerName = gameGreeting($gameDescription);
+    for ($round = 1; $round <= 3;) {
+        $firstNumber = rand(1, 100);
+        $seconfNumber = rand(1, 100);
+        $givenNumbers = (string) $firstNumber . ' ' . (string) $seconfNumber;
+        if ($firstNumber >= $seconfNumber) {
+            $greaterNumber = $firstNumber;
+            $lowerNumber = $seconfNumber;
+        } else {
+            $greaterNumber = $seconfNumber;
+            $lowerNumber = $firstNumber;
+        }
+        $remainder = $greaterNumber % $lowerNumber;
+        while ($remainder !== 0) {
+            $greaterNumber = $lowerNumber;
+            $lowerNumber = $remainder;
+            $remainder = $greaterNumber % $lowerNumber;
+        }
+        $greatestCommonDivisor = (string) $lowerNumber;
+        if (isUserAnswerTrue($playerName, $givenNumbers, $greatestCommonDivisor)) {
+            $round += 1;
+        } else {
+            $round = 1;
+        }
+    }
+    gameEnding($playerName);
 }
