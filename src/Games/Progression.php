b795986b227b2255b3cd3619c3c2ecf617b9b411
kerodim <turboboomer2000@gmail.com>
2020-07-20 15:09:03 +0300

    change brain-progression architecture

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -2,6 +2,10 @@
 
 namespace Braingames\Games\Progression;
 
+use function BrainGames\Cli\gameGreeting;
+use function BrainGames\Cli\isUserAnswerTrue;
+use function BrainGames\Cli\gameEnding;
+
 function showTaskToPlayerProgression()
 {
     return 'What number is missing in the progression?';
@@ -29,4 +33,36 @@ function generationArithmeticProgression()
     }
     $result = [$progression, $correctAnswer];
     return $result;
+}
+
+function prime()
+{
+    $gameDescription = 'What number is missing in the progression?';
+    $playerName = gameGreeting($gameDescription);
+    for ($round = 1; $round <= 3;) {
+        $progressionMember = rand(1, 100);
+        $progressionStep = rand(-5, 5);
+        $progressionLehgth = 10;
+        $unknownMemberNumber = rand(1, $progressionLehgth);
+        $index = 1;
+        $progression = '';
+        for ($index; $index <= $progressionLehgth; $index++) {
+            if ($index === $unknownMemberNumber) {
+                $progression = $progression . '..';
+                $missingMember = (string) $progressionMember;
+            } else {
+                $progression = $progression . (string) $progressionMember;
+            }
+            if ($index < $progressionLehgth) {
+                $progression = $progression . ' ';
+            }
+            $progressionMember = $progressionMember + $progressionStep;
+        }
+        if (isUserAnswerTrue($playerName, $progression, $missingMember)) {
+            $round += 1;
+        } else {
+            $round = 1;
+        }
+    }
+    gameEnding($playerName);
 }
