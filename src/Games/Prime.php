ed510ee7b0d8464e9c7696ecd8b5010a0de3abeb
kerodim <turboboomer2000@gmail.com>
2020-07-28 12:41:38 +0300

    changing the prime game for a new engine

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -2,9 +2,10 @@
 
 namespace Braingames\Games\Prime;
 
-use function BrainGames\Cli\gameGreeting;
-use function BrainGames\Cli\isUserAnswerTrue;
-use function BrainGames\Cli\gameEnding;
+#use function BrainGames\Cli\gameGreeting;
+#use function BrainGames\Cli\isUserAnswerTrue;
+#use function BrainGames\Cli\gameEnding;
+use function BrainGames\Cli\engine;
 
 /*
 function showTaskToPlayerPrime()
@@ -32,7 +33,7 @@ function isPrime($number)
 }
 */
 
-function prime()
+function prime_old()
 {
     $gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
     $playerName = gameGreeting($gameDescription);
@@ -55,4 +56,33 @@ function prime()
         }
     }
     gameEnding($playerName);
+}
+function isPrime($number)
+{
+    $result = true;
+    if ($number === 1) {
+        $result = false;
+    }
+    for ($count = 2; $count <= sqrt($number); $count++) {
+        if ($number % $count === 0) {
+            $result = false;
+            break;
+        }
+    }
+    return $result;
+}
+
+function prime()
+{
+    $gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+    $gameData = [];
+    $maxCorrectAnswerNumber = 3;
+    $separator = ': ';
+    for ($index = 0; $index < $maxCorrectAnswerNumber; $index++) {
+        $number = rand(1, 100);
+        isPrime($number) ? $correctAnswer = 'yes' : $correctAnswer = 'no';
+        $gameData[$index] = $number . $separator . $correctAnswer;
+    }
+    var_dump($gameData);
+    engine($gameDescription, $gameData, $maxCorrectAnswerNumber, $separator);
 }
