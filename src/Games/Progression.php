9a67bcdece2538a28dc0d703e2428ee3670c5554
kerodim <turboboomer2000@gmail.com>
2020-07-28 12:41:52 +0300

    changing the progression game for a new engine

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -2,9 +2,10 @@
 
 namespace Braingames\Games\Progression;
 
-use function BrainGames\Cli\gameGreeting;
-use function BrainGames\Cli\isUserAnswerTrue;
-use function BrainGames\Cli\gameEnding;
+#use function BrainGames\Cli\gameGreeting;
+#use function BrainGames\Cli\isUserAnswerTrue;
+#use function BrainGames\Cli\gameEnding;
+use function BrainGames\Cli\engine;
 
 /*
 function showTaskToPlayerProgression()
@@ -36,8 +37,8 @@ function generationArithmeticProgression()
     return $result;
 }
 */
-
-function progression()
+/*
+function progression_old()
 {
     $gameDescription = 'What number is missing in the progression?';
     $playerName = gameGreeting($gameDescription);
@@ -67,4 +68,34 @@ function progression()
         }
     }
     gameEnding($playerName);
+}
+*/
+
+function progression()
+{
+    $gameDescription = 'What number is missing in the progression?';
+    $gameData = [];
+    $maxCorrectAnswerNumber = 3;
+    $separator = ': ';
+    for ($index = 0; $index < $maxCorrectAnswerNumber; $index++) {
+        $progressionMember = rand(1, 100);
+        $progressionStep = rand(-5, 5);
+        $progressionLehgth = 10;
+        $unknownMemberNumber = rand(1, $progressionLehgth);
+        $progression = '';
+        for ($count = 1; $count <= $progressionLehgth; $count++) {
+            if ($count === $unknownMemberNumber) {
+                $progression = $progression . '..';
+                $missingMember = (string) $progressionMember;
+            } else {
+                $progression = $progression . (string) $progressionMember;
+            }
+            if ($count < $progressionLehgth) {
+                $progression = $progression . ' ';
+            }
+            $progressionMember = $progressionMember + $progressionStep;
+        }
+        $gameData[$index] = $progression . $separator . $missingMember;
+    }
+    engine($gameDescription, $gameData, $maxCorrectAnswerNumber, $separator);
 }
