466cc0698f76920fe0468e4a29bf50c52def02d3
kerodim <turboboomer2000@gmail.com>
2020-09-11 12:45:01 +0300

    raname value

--- src/Games/Progression.php
+++ src/Games/Progression.php
@@ -13,14 +13,14 @@ function runProgressionGame()
     $gameDescription = 'What number is missing in the progression?';
     $gameData = [];
     for ($index = 0; $index < ROUND_OF_NUMBERS; $index++) {
-        $progressionMember = rand(1, 100);
+        $firstProgressionMember = rand(1, 100);
         $progressionStep = rand(-5, 5);
         $progressionLehgth = 10;
         $unknownMemberNumber = rand(1, $progressionLehgth);
-        $missingMember = $progressionMember + $progressionStep * $unknownMemberNumber;
+        $missingMember = $firstProgressionMember + $progressionStep * $unknownMemberNumber;
         $progression = [];
         for ($count = 1; $count <= $progressionLehgth; $count++) {
-            $progression[] = $progressionMember + $progressionStep * $count;
+            $progression[] = $firstProgressionMember + $progressionStep * $count;
         }
         $progression[$unknownMemberNumber - 1] = '..';
         $progressionString = implode(' ', $progression);
